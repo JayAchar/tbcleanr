975ac11af08467228c402fc806641f378887d129
JA-OSX <jay.achar@doctors.org.uk>
2018-03-16 10:13:23 +0100

    added package help file

---
+++ R/TB.funs.R
@@ -0,0 +1,13 @@
+#' TB.funs: A package for cleaning TB data
+#'
+#' The TB.funs package provides three important functions:
+#' date.format, k6.clean.adm and tb.convert 
+#' 
+#' @section TB.funs functions:
+#' date.format
+#' k6.clean.adm
+#' tb.convert
+#' 
+#' @docType package
+#' @name TB.funs
+NULL
