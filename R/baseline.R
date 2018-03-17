dcd7d1135c1c6e04091ff02a189e3e375ed4ffd6
JA-Win <jay.achar@doctors.org.uk>
2018-03-17 11:44:55 +0100

    drafting - check_apid function for checking

--- R/baseline.R
+++ R/baseline.R
@@ -1,7 +1,7 @@
 # Steps
-# 1. Join ID and treatment start date with lab data
-# 2. Filter by presence of start date
-# 3. Check only 2 factor levels of lab result
+# 1. DONE = Join ID and treatment start date with lab data 
+# 2. DONE = Filter by presence of start date
+# 3. DONE = Check only 2 factor levels of lab result
 # 4. Mutate - absolute number of days from treatment start
 # 5. Sort by absolute number of days from treatment start
 # 6. Keep result with smallest number of absolute days from treatment start
@@ -21,9 +21,18 @@ baseline <- function() {
 
 
 # =================================================================
+# temporary function to review outputs
+check_apid <- function() {
+	r <- sample(d[["APID"]], size = 3)
+	z <- d[APID %in% r, ]
+	return(z)
+}
+
+
 # Prepare test data
 library(readr)
 require(data.table)
+require(lubridate)
 set.wd("functions/TB.funs/tests")
 
 a <- read_csv("admission.csv")					# import admission data
@@ -42,9 +51,47 @@ l <- read_csv("laboratory.csv")					# import lab data
 	a <- a[, .(APID, STARTTRE),]
 	l <- l[, .(APID, FIRST, RESULT), ]
 
+# filter contaminated and NA culture results
+	l <- l[!is.na(RESULT), ]
+	l <- l[RESULT != "3", ]
+
 save(a, l, file = "baseline_test.rda")
 
 # =================================================================
 load("baseline_test.rda")
 
-d <- inner_join(a, l, by = "APID")
+d <- as.data.table(inner_join(a, l, by = "APID"))
+
+# check if result variable is binary
+	if (summary(levels(factor(d$RESULT)))[1] != 2) {
+		stop("Result variable must have 2 levels")
+	}
+
+	
+	d$FIRST <- dmy(d$FIRST)
+	d$STARTTRE <- dmy(d$STARTTRE)					# reclassify date variables
+
+	setkey(d, APID, STARTTRE, FIRST)			# set key to sort
+
+# filter out results with no date
+	d <- d[!is.na(FIRST),]
+
+# remove ineligible records due to date
+	d <- d[STARTTRE - FIRST <= 90, ]
+	d <- d[STARTTRE - FIRST >= -7, ]
+
+# absolute days from treatment start to specimen collection
+	d <- d[, abs_days := abs(FIRST - STARTTRE), ]
+
+# remove duplicate rows - id, time from treatment start, result
+	subvar <- c("APID", "abs_days", "RESULT")
+	setkeyv(d, subvar)
+	d <- unique(d)
+	d <- unique(d, by = subvar)
+
+	# HOJD13069
+# identify id & time from treatment start duplicates
+	d <- d[, dupvar := 1L * (.N > 1L), by = c("APID", "abs_days")] 
+
+# remove negative culture if duplicated with positive on same day
+	d <- d[!(dupvar == 1L & RESULT == "1"), ]
