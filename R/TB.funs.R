93e0c10e1a454d6d3db1c7e383d16e8d5506c72f
JA-Win <jay.achar@doctors.org.uk>
2018-05-14 17:04:22 +0200

    init commit k6_hep_fixer()

--- R/TB.funs.R
+++ R/TB.funs.R
@@ -5,10 +5,12 @@
 #' 
 #' @author Jay Achar \email{jay.achar@@doctors.org.uk}
 #' @seealso \code{\link{baseline}}
-#' @seealso \code{\link{date.format}}
+#' @seealso \code{\link{date_format}}
 #' @seealso \code{\link{k6.clean.adm}}
 #' @seealso \code{\link{tb.convert}}
 #' @seealso \code{\link{bentiu_xpert_date}}
+#' @seealso \code{\link{k6_hep_fixer}}
+#' @seealso \code{\link{k6_outcome_fixer}}
 #' 
 #' @docType package
 #' @name TB.funs
---
+++ R/k6_hep_fixer.R
@@ -0,0 +1,52 @@
+#' Fix Koch 6 baseline viral hepatitis variables
+#'
+#' Combine Koch 6 HBV and HCV baseline variables and output  
+#' integer varialbe 
+#' @param x data frame containing Koch 6 HBV and HCV variables
+#' @author Jay Achar \email{jay.achar@@doctors.org.uk}
+#' @seealso \code{\link{TB.funs}}
+#' @export
+#' @examples
+#' \dontrun{
+#' k6_hep_fixer(p)
+#' }
+
+k6_hep_fixer <- function(x) {
+
+# check input
+	if (!(is.data.frame(x))) {
+			stop("input paramter, x, must be a data frame")
+	}
+
+# outcome variables in Koch 6
+	v <- c("hepB", "hepC", "HepB", "HepC")
+
+	if (! all(v %in% names(x))) {
+		warning("Check all viral hepatitis variables present in data frame")
+		return(x)
+	}
+
+# generate empty variables
+	x$hbv <- NA
+	x$hcv <- NA
+
+# aggregate hbv
+	p <- which(x$hepB == 1 | x$HepB == 1)
+	n <- which(x$hepB == 0 & x$HepB == 2)
+	x$hbv[p] <- 1L
+	x$hbv[n] <- 0L
+
+# aggregate hcv
+	p <- which(x$hepC == 1 | x$HepC == 1)
+	n <- which(x$hepC == 0 & x$HepC == 2)
+	x$hcv[p] <- 1L
+	x$hcv[n] <- 0L
+
+# remove original variables
+	x$hepB <- NULL
+	x$HepB <- NULL
+	x$hepC <- NULL
+	x$HepC <- NULL
+
+return(x)
+}
