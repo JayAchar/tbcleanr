b172f10518e164bc90f3ae3a2db7b0a3dce252dc
JA-Win <jay.achar@doctors.org.uk>
2018-05-19 16:01:01 +0200

    init commit subset_vars() - added chechnya myco lab arg

--- R/subset_vars.R
+++ R/subset_vars.R
@@ -1,8 +1,8 @@
-#' Subset Koch 6 admission variables
+#' Subset variables
 #'
-#' Subset Koch 6 admission variables along pre-defined variable  
+#' Subset variables along pre-defined variable  
 #' sets   
-#' @param x data frame containing Koch 6 admission variables
+#' @param x data frame containing variables
 #' @param set define variable set to apply. Values can be "msc500"
 #' @author Jay Achar \email{jay.achar@@doctors.org.uk}
 #' @seealso \code{\link{TB.funs}}
@@ -13,9 +13,9 @@
 #' }
 
 
-k6_adm_subset_vars <- function(x, set = "msc500") {
+subset_vars <- function(x, set = "msc500") {
 # acceptable values for "set" arg
-	s <- c("msc500")
+	s <- c("msc500", "chechnya_myco_lab")
 
 # check input
 	if (!(is.data.frame(x))) {
@@ -42,10 +42,22 @@ k6_adm_subset_vars <- function(x, set = "msc500") {
 					"Ofx", "Cs", "Eto", "PAS", "PAS Na", "Pto", "Amx-Clv",
 					"Bdq", "Cfz", "Clr", "Dld", "hdH", "ImpCln", "Lzd", "Mpm",
 					"dateend", "dateout", "outfirst", "outfirst2013", "2013outcome")
-		x <- subset(x, select = k)
-		return(x)
 	}
 
+# set = chechnya_myco_lab
+	if (set == "chechnya_myco_lab") {
+		k <- c("dbno", "dstno", "dob", "sputum", "dcol1", "dcol2", "dcol3",
+					"micro1", "micro2", "micro3", "dcolxpert1", "dcolxpert2", "xpert1err",
+					"xpert2err", "xpert1res", "xpert2res", "xpert1rif", "xpert2rif",
+					"mgit1d", "mgit2d", "mgit3d", "mgitres",
+					"ms", "mr", "mh", "mz", "me", "mcm", "mam", "mlfx", "lj1d", "lj2d",
+					"lj3d", "ljres", 
+					"ljs", "ljr", "ljh", "ljz", "lje",
+					"ctmicres", "ctmgitres", "ctidres",
+					"cts", "ctr", "cth", "ctz", "cte", "ctcm", "ctam", "ctlfx", "ctmfx",
+					"ctmfx2", "ctlzd", "cthres", "cthrifres", "cthinhres")
+	}
 
+		x <- subset(x, select = k)
 return(x)	
 }
