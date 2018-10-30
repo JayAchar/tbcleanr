f2c56917fa47a97b69e566382a351581da02f855
JA-OSX <jay.achar@doctors.org.uk>
2018-10-30 07:43:03 +0100

    update gender_fixer() for standard adm file naming

--- R/gender_fixer.R
+++ R/gender_fixer.R
@@ -1,77 +1,53 @@
 #' Factorise gender variable
 #'
-#' Take gender variable from data frame and factorise. Use software
-#' to define which data base is being used as the input
+#' Take gender variable from data frame and factorise. Requires that variable names
+#' have been pre-adjusted.  
 #' @param x data frame containing Koch 6 admission variables
-#' @param software define database being used - "koch_6", "epiinfo"
-#' @param rm_orig remove original variables - TRUE or FALSE
 #' @param ... further arguments passed to or from other methods
 #' @author Jay Achar \email{jay.achar@@doctors.org.uk}
 #' @seealso \code{\link{tbcleanr}}
 #' @export
-#' @importFrom assertive assert_is_data.frame
+#' @importFrom assertthat assert_that
 #' @examples
-#' \dontrun{
-#' gender_fixer(p, software = "koch_6")
-#' }
+#' # data from Koch 6 with variable names pre-cleaned
+#' 
+#' x <- tbcleanr:::gender_k6
+#' gender_fixer(x)
+#' 
 
 
-gender_fixer <- function(x, software = c("excel", "koch_6", "epiinfo"),
-							rm_orig = TRUE, ...) {
+gender_fixer <- function(x, ...) {
 
 # check input
-assert_is_data.frame(x)
-
-# check all args
-	software <- match.arg(software)
+        assert_that(is.data.frame(x))
 
 # =================================================================
-# set software specific variables 
-		if (software == "koch_6") {
-			gen_var <- "gender"
-		}	
-		if (software == "epiinfo") {
-			gen_var <- "SEX"
-		}
-		if (software == "excel") {
-			return(x)
-		}
-# =================================================================
-
 # check gender variable present
-	if (! gen_var %in% names(x)) {
+	if (! "gender" %in% names(x)) {
 		stop("Gender variable not present in data frame")
 	}
 
-if (software == "epiinfo") {
+# for EpiInfo data
+if (is.character(x$gender)) {
 # convert character to numeric
-	x[[gen_var]][x[[gen_var]] == "M"] <- 1
-	x[[gen_var]][x[[gen_var]] == "F"] <- 2
+	x$gender[x$gender == "M"] <- 1
+	x$gender[x$gender == "F"] <- 2
 }
 
-if (software == "koch_6") {
+# for Koch 6 data
+if (is.numeric(x$gender)) {
 	# convert gender == not done to NA
-	x[[gen_var]][x[[gen_var]] == 0] <- NA
+	x$gender[x$gender == 0] <- NA
 }
 
 # check levels of gender variable
-	if (! length(table(x[[gen_var]])) == 2) {
+	if (! length(table(x$gender)) == 2) {
 		stop("Gender variable does not have 2 levels")
 	}
 
 # factorise gender variable
-	x[[gen_var]] <- factor(x[[gen_var]], levels = c(1:2),
+	x$gender <- factor(x$gender, levels = c(1:2),
 				labels = c("Male", "Female"))
-
-# standardise nameing of gender variable
-	if (software == "epiinfo") {
-		x$gender <- x[[gen_var]]
-	}
-
-# remove original variables
- 	if (rm_orig %in% c("TRUE", "T") & software == "epiinfo") {
- 		x[, gen_var] <- NULL
- 	}
 
 x
 }
--- tests/testthat/test-r-gender_fixer.R
+++ tests/testthat/test-r-gender_fixer.R
@@ -6,7 +6,7 @@ library(tbcleanr)
                         readRDS()
 
 # generate correct output        
-        k6_output <- gender_fixer(k6_raw, "koch_6")
+        k6_output <- gender_fixer(k6_raw)
 
 test_that("k6 correct", {
   expect_true(all(names(k6_output) %in% c("idnum", "gender")))
@@ -25,7 +25,7 @@ test_that("k6 correct", {
                                   gender = NA_integer_, 
                                   stringsAsFactors = F))
 # generate NA output
-        k6_na_output <- gender_fixer(k6_NA, "koch_6")
+        k6_na_output <- gender_fixer(k6_NA)
 
 test_that("k6 NA correct", {
         expect_equal(nrow(k6_NA), nrow(k6_na_output))
@@ -40,6 +40,54 @@ test_that("k6 NA correct", {
                                   stringsAsFactors = F))
 # test abnormal gender levels
 test_that("k6 abnormal gender", {
-        expect_error(gender_fixer(k6_abn_level, "koch_6"), 
+        expect_error(gender_fixer(k6_abn_level), 
                      "Gender variable does not have 2 levels")
 })
+
+
+## Epiinfo
+# Load data
+epi_raw <- system.file("testdata", "gender_epi.rds", package = "tbcleanr") %>% 
+        readRDS()
+
+# generate correct output        
+epi_output <- gender_fixer(epi_raw)
+
+test_that("epiinfo correct", {
+        expect_true(all(names(epi_output) %in% c("idnum", "gender")))
+        expect_equal(nrow(epi_raw), nrow(epi_output))
+        expect_true(length(unique(epi_raw$idnum)) == length(unique(epi_output$idnum)))
+        expect_true(is.factor(epi_output$gender))
+        expect_true(is.character(epi_output$idnum))
+        expect_equal(levels(epi_output$gender), c("Male", "Female"))
+        expect_true(all(epi_output$gender[epi_raw$gender == "M"] == "Male"))
+        expect_true(all(epi_output$gender[epi_raw$gender == "F"] == "Female"))
+})
+
+
+# generate raw data with NA
+epi_NA <- rbind(epi_raw, 
+               data.frame(idnum = as.character(11), 
+                          gender = NA_character_, 
+                          stringsAsFactors = F))
+# generate NA output
+epi_na_output <- gender_fixer(epi_NA)
+
+test_that("epi NA correct", {
+        expect_equal(nrow(epi_NA), nrow(epi_na_output))
+        expect_equal(levels(epi_output$gender), c("Male", "Female"))
+        expect_true(is.na(epi_na_output$gender[which(is.na(epi_NA$gender))]))
+})
+
+# generate raw data with additional gender level
+epi_abn_level <- rbind(epi_raw, 
+                      data.frame(idnum = as.character(11), 
+                                 gender = as.character("N"), 
+                                 stringsAsFactors = F))
+# test abnormal gender levels
+test_that("epi abnormal gender", {
+        expect_error(gender_fixer(epi_abn_level), 
+                     "Gender variable does not have 2 levels")
+        expect_error(gender_fixer(c("X")))
+})
+
