086c6b779d9ba580e28b17c6eaba334065603905
JA-Win <jay.achar@doctors.org.uk>
2018-11-07 23:17:20 +0000

    add tests()

--- tests/testthat/test-adm_subset.R
+++ tests/testthat/test-adm_subset.R
@@ -18,6 +18,18 @@ k6_raw <- system.file("testdata", "adm_subset_koch6.rds", package = "tbcleanr") 
 koch6 <- adm_subset(k6_raw)
 
 test_that("Koch 6 output ok", {
-        expect_true("koch6" %in% class(koch6))
-        expect_equal(dim(koch6), c(10, 52))
+  expect_true("koch6" %in% class(koch6))
+  expect_equal(dim(koch6), c(10, 52))
+})
+
+
+# remove "koch6" class
+class(k6_raw) <- class(k6_raw)[1:3]
+
+test_that("Messages, warnings and errors", {
+  expect_message(adm_subset(k6_raw), "No adm object class detected")
+  expect_error(adm_subset(c(1:10)), "x is not a data frame")
+  expect_error(adm_subset(epi_raw, add = 1))
+  expect_error(adm_subset(epi_raw, add = "random_text"), "not found")
+  
 })
---
+++ tests/testthat/test-date_format.R
@@ -0,0 +1,11 @@
+context("date_format")
+library(tbcleanr)
+
+epi <- system.file("testdata", "adm_subset_epi.rds", package = "tbcleanr") %>% 
+        readRDS() %>% 
+        date_format()
+
+test_that("correct output", {
+  expect_equal(class(epi$BIRTDATE), "Date")
+  expect_equal(epi$STARTTRE[1], lubridate::dmy("08/01/2004"))
+})
