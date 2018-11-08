39a06a6e1305887d135d259bbc2cffa9af37071a
JA-OSX <jay.achar@doctors.org.uk>
2018-11-08 17:09:28 +0100

    update hiv_fixer() including tests() and testdata

--- R/hiv_fixer.R
+++ R/hiv_fixer.R
@@ -3,80 +3,18 @@
 #' Take HIV variables from data frame and factorise. Use software
 #' to define which data base is being used as the input
 #' @param x data frame containing Koch 6 admission variables
-#' @param software define database being used - "koch_6", "epiinfo"
-#' @param rm_orig remove original variables - TRUE or FALSE
 #' @param ... further arguments passed to or from other methods
 #' @author Jay Achar \email{jay.achar@@doctors.org.uk}
 #' @seealso \code{\link{tbcleanr}}
 #' @importFrom assertthat assert_that
 #' @export
-#' @examples
-#' \dontrun{
-#' hiv_fixer(p, software = "koch_6")
-#' }
 
-hiv_fixer <- function(x, software = c("excel", "koch_6", "epiinfo"),
-						rm_orig = TRUE, ...) {
+hiv_fixer <- function(x, ...) {
 
 # check input
   assert_that(is.data.frame(x))
 
-# check all args
-	software <- match.arg(software)
-
-# =================================================================
-# set software specific variables 
-		if (software == "koch_6") {
-			h_names <- c("HIV", "cdhivenrol")
-			hiv <- "HIV"
-			cdhiv <- "cdhivenrol"
-		}	
-		if (software == "epiinfo") {
-			h_names <- "HIV"
-			hiv <- "HIV"
-		}
-		if (software == "excel") {
-			return(x)
-		}
-# =================================================================
-
-# check variables are present
-	if (! all(h_names %in% names(x))) {
-		stop("Required HIV variables not included in data frame")
-	}
-
-# generate aggregate HIV variable
-			x$hiv <- NA
-
-# if software = "koch_6"
-		if (software == "koch_6") {
-		# all those with a result start as negative
-			x$hiv[x[[hiv]] %in% c(1, 2) | x[[cdhiv]] %in% c(1,2)] <- 0
-			x$hiv[x[[hiv]] == 1 | x[[cdhiv]] == 1] <- 1
-		}
-
-# if software = epiinfo
-		if (software == "epiinfo") {
-	
-			# all those with a result start as negative
-			x$hiv[x[[hiv]] == 2] <- 0
-			x$hiv[x[[hiv]] == 1] <- 1
-
-		}
-
-# check levels of original HIV variable
-			if (! length(table(x$hiv)) <= 2) {
-				stop("HIV variable has greater than 2 levels")
-			}
-
-# factorise gender variable
-	x$hiv <- factor(x$hiv, levels = c(0:1),
-				labels = c("Negative", "Positive"))
-
-# remove original variables
-		 	if (rm_orig %in% c("TRUE", "T")) {
-		 		x[, h_names] <- NULL
-		 	}
-
+# call correct method
+    UseMethod("hiv_fixer", x)
 x
 }
---
+++ R/hiv_fixer.koch6.R
@@ -0,0 +1,50 @@
+#' Factorise Koch 6 HIV variable
+#'
+#' Take HIV variables from data frame and factorise. 
+#' @param x data frame containing Koch 6 admission variables
+#' @param ... further arguments passed to or from other methods
+#' @author Jay Achar \email{jay.achar@@doctors.org.uk}
+#' @seealso \code{\link{tbcleanr}}
+#' @importFrom assertthat assert_that
+#' @importFrom dplyr mutate select
+#' @importFrom magrittr %>% 
+#' @importFrom rlang .data
+
+hiv_fixer.koch6 <- function(x, ...) {
+    
+# check variables are present
+    if (! all(c("hiv", "cdhivenrol") %in% names(x))) {
+        stop("Required HIV variables not included in data frame")
+    }
+    
+# check that levels for HIV variable are correct
+    assert_that(unique(x$cdhivenrol) %in% c(0:5, NA_integer_) %>% all())
+    
+    
+# recode cdhivenrol
+    # 0 = not defined, 1 = positive, 2 = negative, 3 = not done
+    # 4 = unknown, 5 = pending
+    x$cdhivenrol[x$cdhivenrol == 0] <- NA_integer_
+    x$cdhivenrol[x$cdhivenrol %in% c(3:5)] <- NA_integer_
+    x$cdhivenrol[x$cdhivenrol == 2] <- 0L
+
+# recode hiv 
+    # 0 = Unknown, 1 = positive, 2 = negative, 3 = not done, 4 = pending
+    x$hiv[x$hiv %in% c(0, 3:4)] <- NA_integer_
+    x$hiv[x$hiv == 2] <- 0
+    
+# combine known status with baseline hiv result
+    x <- x %>% 
+        mutate(hiv_sum = rowSums(x[c("hiv", "cdhivenrol")], na.rm = T)) %>% 
+        mutate(hiv = ifelse(.data$hiv_sum >= 1, 1, .data$hiv)) %>% 
+        mutate(hiv = ifelse(is.na(.data$hiv), .data$cdhivenrol, .data$hiv)) %>% 
+        select(-.data$hiv_sum, -.data$cdhivenrol)
+
+    
+# factorise gender variable
+    x$hiv <- factor(x$hiv, levels = c(0:1),
+                    labels = c("Negative", "Positive"))    
+    
+    
+    x
+}
