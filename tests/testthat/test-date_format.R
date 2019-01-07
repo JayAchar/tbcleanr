6bea2c88ad4baee349735d5cb5ca070e5c5804bd
JA-OSX <jay.achar@doctors.org.uk>
2019-01-07 21:21:13 +0100

    date_format(), lab_import_checkr() fix for imported xls files - raw data for date_format & tests

--- tests/testthat/test-date_format.R
+++ tests/testthat/test-date_format.R
@@ -1,11 +1,40 @@
 context("test-date_format")
 library(tbcleanr)
 
-epi <- system.file("testdata", "adm_subset_epi.rds", package = "tbcleanr") %>% 
-        readRDS() %>% 
-        date_format()
+chr_str <- system.file("testdata", "date_format_chr_str.rds", package = "tbcleanr") %>% 
+        readRDS()
 
-test_that("correct output", {
-  expect_equal(class(epi$BIRTDATE), "Date")
-  expect_equal(epi$STARTTRE[1], lubridate::dmy("08/01/2004"))
+chr_str_out <- date_format(chr_str)
+
+test_that("dates all character strings output", {
+  expect_equal(dim(chr_str), dim(chr_str_out))
+  expect_equal(names(chr_str), names(chr_str_out))
+  expect_equal(class(chr_str_out$FIRST), "Date")
+  expect_equal(class(chr_str_out$CULRES), "Date")
+  
+})
+
+posix <- system.file("testdata", "date_format_posix.rds", package = "tbcleanr") %>% 
+  readRDS()
+
+posix_out <- date_format(posix)
+
+test_that("dates all posix class", {
+  expect_equal(dim(posix), dim(posix_out))
+  expect_equal(names(posix), names(posix_out))
+  expect_equal(class(posix_out$FIRST), "Date")
+  expect_equal(class(posix_out$CULRES), "Date")
+})
+
+
+chr_posix <- system.file("testdata", "date_format_chr_posix.rds", package = "tbcleanr") %>% 
+  readRDS()
+
+chr_posix_out <- date_format(chr_posix)
+
+test_that("dates mixed posix and characters", {
+  expect_equal(dim(chr_posix), dim(chr_posix_out))
+  expect_equal(names(chr_posix), names(chr_posix_out))
+  expect_equal(class(chr_posix_out$FIRST), "Date")
+  expect_equal(class(chr_posix_out$CULRES), "Date")
 })
