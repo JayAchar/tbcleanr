e991dec9c8ddd5a274f78f9723ec3b0ed2a69461
J Achar <17378875+JayAchar@users.noreply.github.com>
2019-06-02 17:13:18 +0200

    oca-projects look-up table added with tests

--- tests/testthat/test-lookup-tables.R
+++ tests/testthat/test-lookup-tables.R
@@ -3,4 +3,11 @@ library(tbcleanr)
 
 test_that("oca-missions", {
   expect_true(is.character(oca_missions))
+  expect_true(length(unique(oca_missions)) < length(names(oca_missions)))
+})
+
+test_that("oca-projects", {
+  expect_true(is.character(oca_projects))
+  expect_equal(oca_projects[["0"]], "Monrovia")
+  expect_true(length(unique(oca_projects)) < length(names(oca_projects)))
 })
